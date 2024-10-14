5885e64cd1f9ed05929a7ec566a02b32c3bbdb1d
bryanesteban <bryanesteban_123@hotmail.com>
2024-10-14 06:54:34 -0500

    change logger

--- .history/src/main/java/ec/com/corebank/banquito/ErrorManagment/GlobalExceptionHandler_20241014065410.java
+++ .history/src/main/java/ec/com/corebank/banquito/ErrorManagment/GlobalExceptionHandler_20241014065410.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
+import ec.com.corebank.banquito.services.ServicesImp.ClienteServiceImp;
 
 
 @RestControllerAdvice
